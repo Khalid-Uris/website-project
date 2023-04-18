a0110f7b2ce2a727399c355d8049cf1e038f45a1
Khalid <uriskhalid@gmail.com>
2023-04-19 00:56:06 +0500

    add top banner commponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import TopBanner from "./component/TopBanner/TopBanner";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TopBanner />
+    </div>
+  );
 }
 
 export default App;
